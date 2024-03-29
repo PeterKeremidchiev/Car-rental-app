c19af9a3f69b6cc1c6a6b1004452fbf1f677e5bf
Peter Keremidchiev <spartak97@abv.bg>
2024-03-29 20:32:20 +0200

    Car rental app updates

--- car_rental_app/car_rental_app/web/urls.py
+++ car_rental_app/car_rental_app/web/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from car_rental_app.web.views import index, AboutTemplateView, ContactsTemplateView
+from car_rental_app.web.views import search_cars, ContactView
+from car_rental_app.web.views import index, AboutTemplateView
 
 urlpatterns = (
     path('', index, name='index'),
+    path('search/', search_cars, name='search_cars'),
     path('about/', AboutTemplateView.as_view(), name='about'),
-    path('contacts/', ContactsTemplateView.as_view(), name='contacts'),
+    path('contacts/', ContactView.as_view(), name='contacts'),
 )
--- car_rental_app/car_rental_app/web/views.py
+++ car_rental_app/car_rental_app/web/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic as views
+from django.views.generic.edit import FormView
 
+from car_rental_app.bookings.models import Review
+from car_rental_app.cars.forms import CarSearchForm
 from car_rental_app.cars.models import Car
+from car_rental_app.web.forms import ContactForm
 
 
 # Create your views here.
@@ -10,13 +15,46 @@ from car_rental_app.cars.models import Car
 
 def index(request):
     cars = Car.objects.all()
+    form = CarSearchForm()
     context = {
-        'cars': cars
+        'cars': cars,
+        'reviews': Review.objects.all(),
+        'form': form,
+
     }
     return render(request, 'web/index.html', context)
 
 class AboutTemplateView(views.TemplateView):
     template_name = 'web/about.html'
 
-class ContactsTemplateView(views.TemplateView):
+
+class ContactView(FormView):
     template_name = 'web/contacts.html'
+    form_class = ContactForm
+    success_url = reverse_lazy('index')
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
+def search_cars(request):
+    form = CarSearchForm(request.GET)
+    cars = Car.objects.filter(available=True)
+
+    if form.is_valid():
+        make = form.cleaned_data.get('make')
+        model = form.cleaned_data.get('model')
+        year = form.cleaned_data.get('year')
+        cost_range = form.cleaned_data.get('price_per_day')
+
+        if make:
+            cars = cars.filter(make__icontains=make.lower())
+        if model:
+            cars = cars.filter(model__icontains=model.lower())
+        if year:
+            cars = cars.filter(year=year)
+        if cost_range:
+            min_cost, max_cost = cost_range.split('-')
+            cars = cars.filter(price_per_day__range=(min_cost, max_cost))
+
+    return render(request, 'cars/searched_cars_results.html', {'cars': cars, 'form': form})
