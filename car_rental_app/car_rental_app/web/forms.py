524e9120d5d05c1112bf4c297ac2608bef6f0e9f
Peter Keremidchiev <spartak97@abv.bg>
2024-04-24 22:22:33 +0300

    Car rental app changes

--- car_rental_app/car_rental_app/web/forms.py
+++ car_rental_app/car_rental_app/web/forms.py
@@ -9,8 +9,20 @@ class ContactForm(forms.ModelForm):
         fields = ['name', 'email', 'phone', 'message']
 
         widgets = {
-            'name': forms.TextInput(attrs={'placeholder': 'Name', 'class': 'form-control', 'id': 'inputName4'}),
-            'email': forms.EmailInput(attrs={'placeholder': 'Email', 'class': 'form-control', 'id': 'inputEmail4'}),
-            'phone': forms.TextInput(attrs={'placeholder': 'Phone', 'class': 'form-control', 'id': 'inputSubject4'}),
-            'message': forms.Textarea(attrs={'placeholder': 'Message', 'class': 'form-control', 'id': 'inputMessage'}),
+            'name': forms.TextInput(attrs={
+                'placeholder': 'Name',
+                'class': 'form-control',
+                'id': 'inputName4'}),
+            'email': forms.EmailInput(attrs={
+                'placeholder': 'Email',
+                'class': 'form-control',
+                'id': 'inputEmail4'}),
+            'phone': forms.TextInput(attrs={
+                'placeholder': 'Phone',
+                'class': 'form-control',
+                'id': 'inputSubject4'}),
+            'message': forms.Textarea(attrs={
+                'placeholder': 'Message',
+                'class': 'form-control',
+                'id': 'inputMessage'}),
         }
